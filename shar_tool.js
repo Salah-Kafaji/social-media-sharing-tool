67f4db2010af7575394f7091bb69c8ff562d988f
Salah-Kafaji <salah94master@gmail.com>
2023-07-01 17:42:31 +0300

    initialize commit

---
+++ shar_tool.js
@@ -0,0 +1,26 @@
+
+$('.SH-SharBtn').click(function(){
+    $('.addthis_inline_share_toolbox').toggleClass('open_inline_share_toolbox');
+    $('.SH-SharBtn').toggleClass('SH-SharBtnOpen');
+    $('.SH-AIST_C').fadeToggle(300);
+    $('.at-style-responsive .at-share-btn').css('background-color','#20242e');
+    $('.addthis_inline_share_toolbox').fadeToggle(300);
+});
+
+$('.SH-AIST_C').click(function(){
+    
+    $('.addthis_inline_share_toolbox').toggleClass('open_inline_share_toolbox');
+    $('.SH-SharBtn').toggleClass('SH-SharBtnOpen');
+    $('.SH-AIST_C').fadeToggle(300);
+    $('.addthis_inline_share_toolbox').fadeToggle(300);
+
+})
+$('.SH-closeSHicone').click(function(){
+    
+    $('.addthis_inline_share_toolbox').toggleClass('open_inline_share_toolbox');
+    $('.SH-SharBtn').toggleClass('SH-SharBtnOpen');
+    $('.SH-AIST_C').fadeToggle(300);
+    $('.addthis_inline_share_toolbox').fadeToggle(300);
+
+
+})
